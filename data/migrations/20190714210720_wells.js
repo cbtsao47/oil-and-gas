68b2ca23cf9b0783e76e7d215700238e62b112c1
Ben <cbtsao47@gmail.com>
2019-07-21 16:30:56 -0500

    add site_id in wells table

--- data/migrations/20190714210720_wells.js
+++ data/migrations/20190714210720_wells.js
@@ -1,13 +1,16 @@
-
 exports.up = function(knex) {
-  return knex.schema.createTable('wells',table=>{
-      table.increments()
-      table.string('name',50)
-      table.integer('oil_reserve').defaultTo(0)
-      table.integer('gas_reserve').defaultTo(0)
-  })
+  return knex.schema.createTable("wells", table => {
+    table.increments();
+    table.string("name", 50);
+    table.integer("oil_reserve").defaultTo(0);
+    table.integer("gas_reserve").defaultTo(0);
+    table
+      .integer("site_id")
+      .references("id")
+      .inTable("sites");
+  });
 };
 
 exports.down = function(knex) {
-  return knex.schema.dropTableIfExists('wells')
+  return knex.schema.dropTableIfExists("wells");
 };
