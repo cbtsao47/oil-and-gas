8da7319f91bb0cabb3357b27b303566731022bc7
Ben <cbtsao47@gmail.com>
2019-07-21 18:35:51 -0500

    update models

--- models/Extractions.js
+++ models/Extractions.js
@@ -1,16 +1,32 @@
 const Db = require("../data/dbConfig");
-
+const knex = require("knex");
 module.exports = {
-  getExtractionsByWellId: async id => {
-    if (id) {
-      return await Db("extractions as ex")
-        .where({ id })
-        .join("wells", "wells.id", "ex.well_id");
-    }
-    return await Db("extractions as ex").join(
-      "wells",
-      "wells.id",
-      "ex.well_id"
-    );
+  getExtractionsById: async id => {
+    return await Db("extractions as ex")
+      .where({ "ex.id": id })
+      .join("wells as w", "ex.well_id", "=", "w.id")
+      .join("sites as s", "s.id", "w.site_id")
+      .select(
+        "ex.id",
+        "s.name as site",
+        "w.name as well",
+        "ex.date",
+        "ex.oil_amount",
+        "ex.gas_amount"
+      )
+      .first();
+  },
+  getExtractions: async () => {
+    return await Db("extractions as ex")
+      .join("wells as w", "ex.well_id", "=", "w.id")
+      .join("sites as s", "s.id", "w.site_id")
+      .select(
+        "ex.id",
+        "s.name as site",
+        "w.name as well",
+        "ex.date",
+        "ex.oil_amount",
+        "ex.gas_amount"
+      );
   }
 };
--- models/Sites.js
+++ models/Sites.js
@@ -1,10 +1,24 @@
 const Db = require("../data/dbConfig");
 
 module.exports = {
-  getSites: async id => {
-    if (id) {
-      return await Db("sites").where({ id });
+  getSites: async () => {
+    const sites = await Db("sites");
+    const wells = await Db("wells");
+    const result = sites.map(site => {
+      const wellsData = wells.filter(well => well.site_id === site.id);
+      return { ...site, wells: wellsData };
+    });
+    return result;
+  },
+  getSiteById: async id => {
+    let result = null;
+    const site = await Db("sites")
+      .where({ id })
+      .first();
+    const wells = await Db("wells").where({ site_id: id });
+    if (site) {
+      result = { ...site, wells };
     }
-    return await Db("sites");
+    return result;
   }
 };
