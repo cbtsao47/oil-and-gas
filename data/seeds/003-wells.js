8c4ae16eb9ba313eb8210cbabeb897fb489dbe86
Ben <cbtsao47@gmail.com>
2019-07-21 16:31:21 -0500

    update wells seeds and db

--- data/seeds/003-wells.js
+++ data/seeds/003-wells.js
@@ -1,15 +1,20 @@
-
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('wells').truncate()
-    .then(function () {
+  return knex("wells")
+    .truncate()
+    .then(function() {
       // Inserts seed entries
-      return knex('wells').insert([
-        {id: 1, name: 'Alpha'},
-        {id: 2, name: 'Bravo'},
-        {id: 3, name: 'Charlie'},
-        {id: 4, name: 'Delta'},
-        {id: 5, name:'Echo'}
+      return knex("wells").insert([
+        { id: 1, name: "Alpha", site_id: 1 },
+        { id: 2, name: "Bravo", site_id: 2 },
+        { id: 3, name: "Charlie", site_id: 3 },
+        { id: 4, name: "Delta", site_id: 4 },
+        { id: 5, name: "Echo", site_id: 5 },
+        { id: 6, name: "Alpha", site_id: 1 },
+        { id: 7, name: "Bravo", site_id: 2 },
+        { id: 8, name: "Charlie", site_id: 3 },
+        { id: 9, name: "Delta", site_id: 4 },
+        { id: 10, name: "Echo", site_id: 5 }
       ]);
     });
 };
