628c2e15217a1a66008e14fcf5b0778bf18f3c5e
Ben <cbtsao47@gmail.com>
2019-07-21 18:24:34 -0500

    add reserve routes by id

--- api/routes/reserveRoute.js
+++ api/routes/reserveRoute.js
@@ -1,12 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const sitesModel = require("../../models/Sites");
+
+const serverError = res =>
+  res.status(500).json({ message: "Internal Server Error" });
 
 router.get("/", async (req, res) => {
   try {
-    res.json({ message: "it works" });
+    const sites = await sitesModel.getSites();
+    if (sites.length) res.status(200).json(sites);
   } catch (err) {
-    console.log({ err });
-    res.status(500).json({ message: "Internal Server Error" });
+    serverError(res);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const site = await sitesModel.getSiteById(id);
+    if (site) {
+      res.status(200).json(site);
+    } else {
+      res.status(404).json({ message: "site does not exist" });
+    }
+  } catch (err) {
+    serverError(res);
   }
 });
 
