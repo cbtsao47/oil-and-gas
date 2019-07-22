92d57e702e7b32ef3a67ce8c41ae9584ee8b3ecb
Ben <cbtsao47@gmail.com>
2019-07-22 03:58:46 -0500

    update extraction model

--- models/Extractions.js
+++ models/Extractions.js
@@ -17,16 +17,25 @@ module.exports = {
       .first();
   },
   getExtractions: async () => {
-    return await Db("extractions as ex")
+    const sites = await Db("sites");
+    const extractions = await Db("extractions as ex")
       .join("wells as w", "ex.well_id", "=", "w.id")
       .join("sites as s", "s.id", "w.site_id")
       .select(
         "ex.id",
-        "s.name as site",
+        "s.id as site",
         "w.name as well",
         "ex.date",
         "ex.oil_amount",
         "ex.gas_amount"
+      )
+      .orderBy("well");
+    const result = sites.map(site => {
+      const extractionsData = extractions.filter(
+        extraction => extraction.site === site.id
       );
+      return { ...site, wells: extractionsData };
+    });
+    return result;
   }
 };
